5ee09b7552712cfb43b569cb7628f5557554efb9
Peter Lourens <peter@lourens.cloud>
2024-10-29 06:45:32 +0000

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,1 +1,21 @@
-#
+import secrets.py
+
+def do_connect(ssid, pwd):
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        led.value(1)
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(ssid, pwd)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+    led.value(0)
+ 
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+ 
+# Attempt to connect to WiFi network
+do_connect(secrets.SSID, secrets.WIFI_PASS)
